4a1909ffd79fc16d6bf94d74bef6a5eee116faaf
Nejc Povšič <npovsic@gmail.com>
2019-12-01 19:40:14 +0100

    Added info.

--- routes/infoRoutes.js
+++ routes/infoRoutes.js
@@ -1,7 +1,38 @@
 const initRoutes = function (server) {
     server.get('/info', function (req, res, next) {
         res.json({
-            members: ['np9417', 'lb']
+            "clani": ["np9417", ""],
+            "opis_projekta": "Projekt je namenjen iskanju poti preko LPP-ja",
+            "mikrostoritve": [],
+            "github": [
+                "https://github.com/BezPov/lpp-api-gateway",
+                "https://github.com/BezPov/rso-lpp-logger",
+                "https://github.com/BezPov/rso-lpp-route-finder",
+                "https://github.com/BezPov/rso-lpp-payment",
+                "https://github.com/BezPov/rso-lpp-parser",
+                "https://github.com/BezPov/rso-lpp-account",
+                "https://github.com/BezPov/rso-lpp-frontend"
+            ],
+            "travis": [
+                "https://travis-ci.org/BezPov/rso-lpp-feedback",
+                "https://travis-ci.org/BezPov/rso-lpp-logger",
+                "https://travis-ci.org/BezPov/lpp-api-gateway",
+                "https://travis-ci.org/BezPov/rso-lpp-account",
+                "https://travis-ci.org/BezPov/rso-lpp-frontend",
+                "https://travis-ci.org/BezPov/rso-lpp-route-finder",
+                "https://travis-ci.org/BezPov/rso-lpp-payment",
+                "https://travis-ci.org/BezPov/rso-lpp-parser"
+            ],
+            "dockerhub": [
+                "https://hub.docker.com/repository/docker/bezpov/logger",
+                "https://hub.docker.com/repository/docker/bezpov/lpp-api-gateway",
+                "https://hub.docker.com/repository/docker/bezpov/lpp-account",
+                "https://hub.docker.com/repository/docker/bezpov/lpp-frontend",
+                "https://hub.docker.com/repository/docker/bezpov/lpp-route-finder",
+                "https://hub.docker.com/repository/docker/bezpov/lpp-payment",
+                "https://hub.docker.com/repository/docker/bezpov/lpp-parser",
+                "https://hub.docker.com/repository/docker/bezpov/lpp-feedback"
+            ]
         });
 
         return next();
